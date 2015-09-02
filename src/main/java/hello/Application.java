a5c6ee53e66d9db2797f7860efbc45ffb17fc240
Katyushka <katya94fm@mail.ru>
2015-09-02 22:41:24 +0500

    add spring boot security

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,15 +13,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
     }
 }
